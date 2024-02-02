e98e1172e261879ad2e0f49238dd2fef2681dcc0
zntb <zenetibi@yahoo.com>
2024-02-02 13:15:40 +0200

    Get Token Prices (Frontend)

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -6,6 +6,7 @@ import {
   SettingOutlined,
 } from '@ant-design/icons';
 import tokenList from '../tokenList.json';
+import axios from 'axios';
 
 function Swap() {
   const [slippage, setSlippage] = useState(2.5);
@@ -15,6 +16,7 @@ function Swap() {
   const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
   const [isOpen, setIsOpen] = useState(false);
   const [changeToken, setChangeToken] = useState(1);
+  const [prices, setPrices] = useState(null);
 
   function handleSlippageChange(e) {
     setSlippage(e.target.value);
@@ -22,13 +24,22 @@ function Swap() {
 
   function changeAmount(e) {
     setTokenOneAmount(e.target.value);
+    if (e.target.value && prices) {
+      setTokenTwoAmount((e.target.value * prices.ratio).toFixed(2));
+    } else {
+      setTokenTwoAmount(null);
+    }
   }
 
   function switchTokens() {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
     const one = tokenOne;
     const two = tokenTwo;
     setTokenOne(two);
     setTokenTwo(one);
+    fetchPrices(two.address, one.address);
   }
 
   function openModal(asset) {
@@ -37,13 +48,31 @@ function Swap() {
   }
 
   function modifyToken(i) {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
     if (changeToken === 1) {
       setTokenOne(tokenList[i]);
+      fetchPrices(tokenList[i].address, tokenTwo.address);
     } else {
       setTokenTwo(tokenList[i]);
+      fetchPrices(tokenOne.address, tokenList[i].address);
     }
     setIsOpen(false);
   }
+
+  async function fetchPrices(one, two) {
+    const res = await axios.get('http://localhost:3001/tokenPrice', {
+      params: { addressOne: one, addressTwo: two },
+    });
+
+    console.log(res.data);
+    setPrices(res.data);
+  }
+
+  useEffect(() => {
+    fetchPrices(tokenList[0].address, tokenList[1].address);
+  }, []);
 
   const settings = (
     <>
@@ -101,7 +130,7 @@ function Swap() {
             placeholder="0"
             value={tokenOneAmount}
             onChange={changeAmount}
-            // disabled={!prices}
+            disabled={!prices}
           />
           <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
           <div className="switchButton" onClick={switchTokens}>
