e498980f65768e78f3a0992d1cf4a418ec56ae59
zntb <zenetibi@yahoo.com>
2024-02-02 08:54:26 +0200

    Add Header

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Swap from './components/Swap';
+import Tokens from './components/Tokens';
+
+import './App.css';
 
 function App() {
-  return <div className="App">Dex</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="mainWindow">
+        <Routes>
+          <Route path="/" element={<Swap />} />
+          <Route path="/tokens" element={<Tokens />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
