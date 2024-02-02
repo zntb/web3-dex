40d07f130f20432124337ab274dc98362e89bf4b
zntb <zenetibi@yahoo.com>
2024-02-02 09:20:02 +0200

    Add Swap Page

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Input, Popover, Radio, Modal, message } from 'antd';
+import {
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
 
 function Swap() {
+  const [slippage, setSlippage] = useState(2.5);
+  const [tokenOneAmount, setTokenOneAmount] = useState(null);
+  const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+
+  function handleSlippageChange(e) {
+    setSlippage(e.target.value);
+  }
+
+  function changeAmount(e) {
+    setTokenOneAmount(e.target.value);
+  }
+
+  const settings = (
+    <>
+      <div>Slippage Tolerance</div>
+      <div>
+        <Radio.Group value={slippage} onChange={handleSlippageChange}>
+          <Radio.Button value={0.5}>0.5%</Radio.Button>
+          <Radio.Button value={2.5}>2.5%</Radio.Button>
+          <Radio.Button value={5}>5.0%</Radio.Button>
+        </Radio.Group>
+      </div>
+    </>
+  );
+
   return (
-    <div>Swap</div>
-  )
+    <div className="tradeBox">
+      <div className="tradeBoxHeader">
+        <h4>Swap</h4>
+        <Popover
+          content={settings}
+          title="Settings"
+          trigger="click"
+          placement="bottomRight"
+        >
+          <SettingOutlined className="cog" />
+        </Popover>
+      </div>
+      <div className="inputs">
+        <Input
+          placeholder="0"
+          value={tokenOneAmount}
+          onChange={changeAmount}
+          // disabled={!prices}
+        />
+        <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
+      </div>
+    </div>
+  );
 }
 
-export default Swap
+export default Swap;
